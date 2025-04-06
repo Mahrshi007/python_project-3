04296d1341d5910c92fa9df7c7e3e2b4d657981a
Mahrshi007 <mnpatel0304@gmail.com>
2025-04-06 14:57:11 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,14 @@
+import math
+n=int(input("Enter a number: "))
+def sqrt_num(n):
+    a=math.sqrt(n)
+    print("Square root: ",a)
+sqrt_num(n)
+def log(n):
+    b = math.log(n)
+    print("Logarithm: ",b)
+log(n)
+def sine(n):
+    c = math.sin(n)
+    print("Sine: " ,c)
+sine(n)
